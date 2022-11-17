104a9c5417639e98c8403f078794d40a4c3694d9
CHENEAU LEO <leo.cheneau@etu.unistra.fr>
2022-11-17 13:25:23 +0000

    Les tables SQL ont quelques petits problèmes

--- tables.sql
+++ tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE utilisateurs
     nom VARCHAR(20),
     prenom VARCHAR(20),
     login VARCHAR(20),
-    pass VARCHAR(20),
+    hpass BINARY(64),
     privileges INT
 );
 
@@ -19,6 +19,30 @@ CREATE TABLE entrepot
 (
     id_entrepot INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     nom VARCHAR(10)
+);
+
+CREATE TABLE allee
+(
+    id_allee INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    allee VARCHAR(4)
+);
+
+CREATE TABLE travee
+(
+    id_travee INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    travee VARCHAR(4)
+);
+
+CREATE TABLE niveau
+(
+    id_niveau INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    niveau VARCHAR(4)
+);
+
+CREATE TABLE alveole
+(
+    id_alveole INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    alveole VARCHAR(4)
 );
 
 
@@ -37,8 +61,9 @@ CREATE TABLE entrepot_site
     FOREIGN KEY (id_alveole) REFERENCES alveole(id_alveole)
 );
 
-CREATE TABLE stocked
+CREATE TABLE stock
 (
+    id_stock INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     quantity INT NOT NULL,
     id_site INT NOT NULL,
     id_article INT NOT NULL,
@@ -46,27 +71,15 @@ CREATE TABLE stocked
     FOREIGN KEY (id_article) REFERENCES article(id_article)
 );
 
-CREATE TABLE allee
+CREATE TABLE transactions
 (
-    id_allee INT PRIMARY NOT NULL,
-    allee VARCHAR(4)
-);
-
-CREATE TABLE travee
-(
-    id_travee INT PRIMARY NOT NULL,
-    travee VARCHAR(4)
-);
-
-CREATE TABLE niveau
-(
-    id_niveau INT PRIMARY NOT NULL,
-    niveau VARCHAR(4)
-);
-
-
-CREATE TABLE alveole
-(
-    id_alveole INT PRIMARY NOT NULL,
-    alveole VARCHAR(4)
+	id_transaction INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	id_utilisateur INT NOT NULL,
+	id_article INT NOT NULL,
+	id_site INT NOT NULL,
+	delta INT NOT NULL,
+	estampille DATETIME NOT NULL,
+    	FOREIGN KEY (id_utilisateur) REFERENCES utilisateurs(id_utilisateur),
+	FOREIGN KEY (id_article) REFERENCES article(id_article),
+	FOREIGN KEY (id_site) REFERENCES entrepot_site(id_site)
 );
