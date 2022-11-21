6920758b298916ad1f0c845c069b97a10a05fd9e
zeFresk <8807862+zeFresk@users.noreply.github.com>
2022-11-21 18:26:45 +0100

    no <br>

--- static/auth.php
+++ static/auth.php
@@ -18,11 +18,11 @@ $username = get_var($_POST, 'username');
 $password = get_var($_POST, 'password');
 
 /* DEBUG */
-echo("DEBUG: _POST = <br>");
+echo("DEBUG: _POST = ");
 var_dump($_POST);
-echo("<br>DEBUG: username: ");
+echo("DEBUG: username: ");
 var_dump($username);
-echo("<br>DEBUG: password: ");
+echo("DEBUG: password: ");
 var_dump($password);
 
 if ($username == '') {
@@ -45,13 +45,13 @@ $prep->bindValue('username', $username);
 $prep->bindValue('hpass', $hashed_password);
 
 /* DEBUG */
-echo("<br>DEBUG: salted password: ");
+echo("DEBUG: salted password: ");
 var_dump($salted_password);
-echo("<br>DEBUG: hashed password: ");
+echo("DEBUG: hashed password: ");
 var_dump($hashed_password);
-echo("<br>DEBUG: hashed password (as hex): ");
+echo("DEBUG: hashed password (as hex): ");
 var_dump(bin2hex($hashed_password));
-echo("<br>DEBUG: hexa hash: ");
+echo("DEBUG: hexa hash: ");
 var_dump(hash('sha3-512', $salted_password, false));
 
 /* Exécution */
