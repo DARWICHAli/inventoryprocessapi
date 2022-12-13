9f6976e530ec4ed356acbb55a799046320984aaf
zeFresk <8807862+zeFresk@users.noreply.github.com>
2022-12-13 18:18:36 +0100

    Fix headers + session_start + auto-indent

--- static/query.php
+++ static/query.php
@@ -1,125 +1,123 @@
 <?php
-    // Headers
-    header('Content-Type: application/json');
+require_once 'include/config.php';
+include_once 'include/QueryClass.php';
+include_once 'include/utils.php';
+require_once 'include/bdd.php';
 
-    include_once 'include/QueryClass.php';
-    include_once 'include/utils.php';
+global $pdo;
 
-    require_once 'include/config.php';
-    require_once 'include/bdd.php';
+// Instantiate blog post object
+$post = new QueryClass($pdo);
 
-    global $pdo;
+// Receive JSON file and converts it into a PHP object
+$data = json_decode(file_get_contents('php://input'), true);
 
-    // Instantiate blog post object
-    $post = new QueryClass($pdo);
+// Verify valid JSON format
+if (json_last_error() !== JSON_ERROR_NONE) {
+    raise_http_error("Invalid JSON format \n", 400);
+}
 
-    // Receive JSON file and converts it into a PHP object
-    $data = json_decode(file_get_contents('php://input'), true);
+// Verify and parse request
+try {
+    $post->verify_and_parse_request($data);
+}
+catch(Exception $e) {
+    raise_http_error($e->getMessage(), $e->getCode());
+}
 
-    // Verify valid JSON format
-    if (json_last_error() !== JSON_ERROR_NONE) {
-        raise_http_error("Invalid JSON format \n", 400);
-    }
+// Verify token
+try{
+    $post->verify_token($data);
+}
+catch(Exception $e){
+    raise_http_error($e->getMessage(), $e->getCode());
+}
 
-    // Verify and parse request
-    try {
-        $post->verify_and_parse_request($data);
-    }  
-    catch(Exception $e) {
-        raise_http_error($e->getMessage(), $e->getCode());
-    }
+// Execute query
+$response = null;
+switch($post->get_code()){
 
-    // Verify token
-    try{
-        $post->verify_token($data);
-    }
-    catch(Exception $e){
-        raise_http_error($e->getMessage(), $e->getCode());
-    }
+    case 1:
+        // verify query
+        try{
+            verify_insert_query();
+        }
+        catch(Exception $e){
+            raise_http_error($e->getMessage(), $e->getCode());
+        }
 
-    // Execute query 
-    $response = null;
-    switch($post->get_code()){
+        // execute query
+        try {
+            $post->insert();
+        }
+        catch(Exception $e){
+            raise_http_error($e->getMessage(), $e->getCode());
+        }
+        break;
 
-        case 1:
-            // verify query
-            try{
-                verify_insert_query();
-            }
-            catch(Exception $e){
-                raise_http_error($e->getMessage(), $e->getCode());
-            }
+    case 3:
+        // verify query
+        try{
+            verify_warehouses_query();
+        }
+        catch(Exception $e){
+            raise_http_error($e->getMessage(), $e->getCode());
+        }
 
-            // execute query
-            try {
-                $post->insert();
-            }
-            catch(Exception $e){
-                raise_http_error($e->getMessage(), $e->getCode());
-            }
-            break;
+        // execute query
+        try{
+            $response = $post->getWarehouses();
+        }
+        catch(Exception $e){
+            raise_http_error($e->getMessage(), $e->getCode());
+        }
+        break;
 
-        case 3:
-            // verify query
-            try{
-                verify_warehouses_query();
-            }
-            catch(Exception $e){
-                raise_http_error($e->getMessage(), $e->getCode());
-            }
+    case 5:
+        // verify query
+        try {
+            verify_products_query();
+        }
+        catch(Exception $e){
+            raise_http_error($e->getMessage(), $e->getCode());
+        }
 
-            // execute query
-            try{
-                $response = $post->getWarehouses();
-            }
-            catch(Exception $e){
-                raise_http_error($e->getMessage(), $e->getCode());
-            }
-            break;
+        // execute query
+        try{
+            $response = $post->getProducts();
+        }
+        catch(Exception $e){
+            raise_http_error($e->getMessage(), $e->getCode());
+        }
+        break;
 
-        case 5:
-            // verify query
-            try {
-                verify_products_query();
-            }
-            catch(Exception $e){
-                raise_http_error($e->getMessage(), $e->getCode());
-            }
+    case 7:
+        // verify query
+        try {
+            verify_update_query();
+        }
+        catch (Exception $e){
+            raise_http_error($e->getMessage(), $e->getCode());
+        }
 
-            // execute query
-            try{
-                $response = $post->getProducts();
-            }
-            catch(Exception $e){
-                raise_http_error($e->getMessage(), $e->getCode());
-            }
-            break;
+        // execute query
+        try{
+            $response = $post->update();
+        }
+        catch(Exception $e){
+            raise_http_error($e->getMessage(), $e->getCode());
+        }
+        break;
 
-        case 7:
-            // verify query
-            try {
-                verify_update_query();
-            }
-            catch (Exception $e){
-                raise_http_error($e->getMessage(), $e->getCode());
-            }
+    default:
+        break;
+}
 
-            // execute query
-            try{
-                $response = $post->update();
-            }
-            catch(Exception $e){
-                raise_http_error($e->getMessage(), $e->getCode());
-            }
-            break;
+// Json Encoding
+// Headers
+header('Content-Type: application/json');
+echo json_encode($response);
 
-        default:
-            break;
-    }
-
-    // Json Encoding
-    echo json_encode($response);
-
-    // success
-    die(200);
+// success
+die(200);
 ?>
